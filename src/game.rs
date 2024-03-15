6941dc3f7f1206750bd23966a7ba507b88added4
Connor Milligan <connoramilligan@gmail.com>
2024-03-15 14:47:37 -0400

    hacky async

--- src/game.rs
+++ src/game.rs
@@ -1,29 +1,58 @@
-use std::io;
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use std::{io::{self, Write}, thread::sleep, time::Duration};
+use crossterm::{
+    event::{self, poll, Event, KeyCode, KeyEvent, KeyEventKind},
+    QueueableCommand,
+};
 
-use crate::menu::draw_menu;
+use crate::menu::{clear_canvas, draw_menu};
 
 pub struct Game {
     pub stdout: std::io::Stdout,
     pub running: bool,
+    pub tps: u16,
+    pub tick: u16,
+    pub test: (u16, u16),
 }
 
 pub fn game_loop() -> io::Result<()> {
     let mut game = Game {
         stdout: std::io::stdout(),
         running: true,
+        tps: 10,
+        tick: 0,
+        test: (1, 5),
     };
 
     while game.running {
-        draw(&mut game)?;
         take_input(&mut game)?;
+        tick(&mut game)?;
+        draw(&mut game)?;
     }
 
     Ok(())
 }
 
 fn draw(game: &mut Game) -> io::Result<()> {
+    clear_canvas(game)?;
     draw_menu(game)?;
+    game.stdout.queue(crossterm::cursor::MoveTo(game.test.0, game.test.1))?
+        .queue(crossterm::style::Print("X"))?
+        .flush()?;
+    Ok(())
+}
+
+fn tick(game: &mut Game) -> io::Result<()> {
+    if game.tick < game.tps {
+        game.tick += 1;
+    }
+    else {
+        game.tick = 0;
+        if game.test.0 < 20 {
+            game.test.0 += 1;
+        } else {
+            game.test.0 = 1;
+        }
+    }
     Ok(())
 }
 
@@ -32,6 +61,16 @@ fn take_input(game: &mut Game) -> io::Result<()> {
         'q' => {
             game.running = false;
         }
+        '>' => {
+            if game.tps < 10 {
+                game.tps += 1;
+            }
+        }
+        '<' => {
+            if game.tps > 1 {
+                game.tps -= 1;
+            }
+        }
         _ => {}
     }
     Ok(())
@@ -39,14 +78,19 @@ fn take_input(game: &mut Game) -> io::Result<()> {
 
 fn read_char() -> std::io::Result<char> {
     loop {
-        if let Ok(Event::Key(KeyEvent {
-            code: KeyCode::Char(c),
-            kind: KeyEventKind::Press,
-            modifiers: _,
-            state: _,
-        })) = event::read()
-        {
-            return Ok(c);
+        if poll(Duration::from_millis(100))? {
+            if let Ok(Event::Key(KeyEvent {
+                code: KeyCode::Char(c),
+                kind: KeyEventKind::Press,
+                modifiers: _,
+                state: _,
+            })) = event::read()
+            {
+                return Ok(c);
+            }
+        }
+        else {
+            return Ok('~');
         }
     }
 }
--- src/menu.rs
+++ src/menu.rs
@@ -1,8 +1,26 @@
-use std::{fmt::format, io::{self, Write}};
-use crossterm::{cursor, queue, style::{self, Colors, Print, PrintStyledContent, SetColors, SetForegroundColor}, QueueableCommand};
+use std::io::{self};
+use crossterm::{cursor, style::{self, Colors, Print, SetColors, SetForegroundColor}, QueueableCommand};
 use crate::game::Game;
 
-const MENU_OPTIONS: [(&str, &str); 1] = [("q", "Quit the game")];
+const MENU_OPTIONS: [(&str, &str); 3] = [
+    ("q", "Quit the game"),
+    ("<", "Decrease TPS"),
+    (">", "Increase TPS")
+];
+
+pub fn clear_canvas(game: &mut Game) -> io::Result<()> {
+    let (width, height) = crossterm::terminal::size()?;
+
+    for h in 1..height-1 {
+        for w in 1..width-width/6-1 {
+            if game.test.0 != w && game.test.1 != h {
+                game.stdout.queue(cursor::MoveTo(w, h))?
+                    .queue(Print(" "))?;
+            }
+        }
+    }
+    Ok(())
+}
 
 pub fn draw_menu(game: &mut Game) -> io::Result<()> {
     let (width, height) = crossterm::terminal::size()?;
@@ -16,14 +34,16 @@ pub fn draw_menu(game: &mut Game) -> io::Result<()> {
             .queue(Print("█"))?; 
     }
     for w in 0..width {
-        game.stdout.queue(cursor::MoveTo(w, 0))?
-            .queue(Print("█"))?
-            .queue(cursor::MoveTo(w, height))?
+        if w < 2 || w >= 16 {
+            game.stdout.queue(cursor::MoveTo(w, 0))?
+                .queue(Print("█"))?;
+        }
+        game.stdout.queue(cursor::MoveTo(w, height))?
             .queue(Print("█"))?;
     }
     
     // Draw the menu
-    for h in 0..height {
+    for h in 1..height-1 {
         game.stdout.queue(cursor::MoveTo(0, h))?
             .queue(cursor::MoveTo(width-width/6, h))?
             .queue(Print("█"))?; 
@@ -32,7 +52,7 @@ pub fn draw_menu(game: &mut Game) -> io::Result<()> {
     // Draw the title
     game.stdout.queue(cursor::MoveTo(2, 0))?
         .queue(SetColors(Colors::new(style::Color::Black, style::Color::Grey)))?
-        .queue(Print("Game of Life"))?;
+        .queue(Print(" Game of Life "))?;
 
     // Draw the options
     for (i, (key, description)) in MENU_OPTIONS.iter().enumerate() {
@@ -45,9 +65,10 @@ pub fn draw_menu(game: &mut Game) -> io::Result<()> {
             
     }
 
-
+    game.stdout.queue(cursor::MoveTo(width-10, 0))?
+        .queue(SetColors(Colors::new(style::Color::Yellow, style::Color::DarkGrey)))?
+        .queue(Print(format!(" TPS: {} ", game.tps)))?;
 
     game.stdout.queue(SetColors(Colors::new(style::Color::White, style::Color::Black)))?;
-
-    game.stdout.flush()
+    Ok(())
 }
